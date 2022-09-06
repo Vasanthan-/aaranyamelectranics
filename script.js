511237b65aa8dfea41bb0a40fc9984b951a14afa
Vasanthan <vasanthancarbon@gmail.com>
2022-09-06 14:25:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ function onClick(element) {
     var captionText = document.getElementById("caption");
     captionText.innerHTML = element.alt;
 }
+
+// Interactive Form
+function selection(s1, s2) {
+    var s1 = document.getElementById(s1);
+    var s2 = document.getElementById(s2);
+
+    s2.innerHTML = "";
+    if (s1.value == "AMD") {
+        var optionArray = ["select cpu|Select CPU", "ryzen 3|Ryzen 3", "ryzen 5|Ryzen 5"];
+    } else if (s1.value == "Intel") {
+        var optionArray = ["select cpu|Select CPU", "core i3|Core i3", "core i5|Core i5"];
+    }
+    for (var option in optionArray) {
+        var pair = optionArray[option].split("|");
+        var newOption = document.createElement("option");
+        newOption.value = pair[0];
+        newOption.innerHTML = pair[1];
+        s2.options.add(newOption);
+    }
+}
